732f63f33c7d36904c85973b07fa2e51199393f8
Chloe <twinklefan7@gmail.com>
2025-11-08 13:03:10 +1300

    Add Firebase messaging service worker and update manifest for notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+// Import Firebase scripts
+importScripts("https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/12.5.0/firebase-messaging.js");
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyBDQVLVHlNMtit_X2lrtNp5_nqVaY7vWY0",
+  projectId: "avaliability",
+  messagingSenderId: "163231772068",
+  appId: "1:163231772068:web:291a0be7ec337e45632531"
+});
+
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage(function (payload) {
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "check.png",
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
